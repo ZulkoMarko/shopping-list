ed99571f9123214a5b17ddec01b562d787241a35
Markic <marko1998.marac@gmail.com>
2025-02-25 17:23:28 +0100

    shopping list

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 const itemForm=document.getElementById("item-form");
 const itemInput=document.getElementById("item-input");
 const itemList=document.getElementById("item-list");
-const newItem=itemInput.value;
+
+
 function addItem(e){
 e.preventDefault();
-  
 
-const li = document.createElement(`li`);
+  const newItem = itemInput.value;
+
+//Validate input
+if(newItem === ""){alert("Please add an item");
+  return;
+}
+
+//Create li
+const li=document.createElement("li");
 li.appendChild(document.createTextNode(newItem));
-console.log(li);
-
-const button=createButton(`remove-item btn-link text-red`);
+const button=createButton("remove-item btn-link text-red");
 li.appendChild(button);
 itemList.appendChild(li);
-itemInput.value="";
+
+itemInput.value= "";
 }
 
 function createButton(classes){
-  const button=document.createElement("button");
+  const button= document.createElement("button");
   button.className= classes;
-  const icon= createIcon(`fa-solid fa-xmark`);
+  const icon=createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
   return button;
 }
 
 function createIcon(classes){
-  const icon=document.createElement(`i`);
-  icon.className=classes;
-  return icon;
+const icon=document.createElement("i");
+icon.className=classes;
+return icon;
 }
 
 //Event listener
 
-itemForm.addEventListener(`submit`, addItem);
+itemForm.addEventListener("submit", addItem);
